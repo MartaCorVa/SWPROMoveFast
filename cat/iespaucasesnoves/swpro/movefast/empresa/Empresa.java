11212115e71d131711dcf70419c26814aaaedfa8
maria <maria@gmail.com>
2019-05-06 19:18:03 +0200

    Lloguer

--- cat/iespaucasesnoves/swpro/movefast/empresa/Empresa.java
+++ cat/iespaucasesnoves/swpro/movefast/empresa/Empresa.java
@@ -3,12 +3,92 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cat.iespaucasesnoves.swpro.movefast.empresa;
+package cat.iespaucasesnoves.swpro.movefast.Empresa;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
 
 /**
  *
  * @author Miquel
  */
 public class Empresa {
+    private String nom;
+    private String id;
+    private String adreça;
+    private HashMap<String, Leasing> leasings;
+
+    public Empresa(String nom, String id, String adreça) {
+        this.nom = nom;
+        this.id = id;
+        this.adreça = adreça;
+        leasings = new HashMap();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAdreça() {
+        return adreça;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAdreça(String adreça) {
+        this.adreça = adreça;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" + "nom=" + nom + ", id=" + id + ", adre\u00e7a=" + adreça + ", leasings=" + leasings + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.nom);
+        hash = 79 * hash + Objects.hashCode(this.id);
+        hash = 79 * hash + Objects.hashCode(this.adreça);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Empresa other = (Empresa) obj;
+        if (!Objects.equals(this.nom, other.nom)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return Objects.equals(this.adreça, other.adreça);
+    }
+
+    public ArrayList<Leasing> getLeasings() {
+        return leasings;
+    }
+    
+    public void afegirLeasing(Leasing leasing){
+        if (client.getTipus() == TipusClient.LEASING) {
+            
+        }
+    }
     
 }
---
+++ cat/iespaucasesnoves/swpro/movefast/lloguer/Lloguer.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.iespaucasesnoves.swpro.movefast.lloguer;
+
+import cat.iespaucasesnoves.swpro.movefast.client.Client;
+import cat.iespaucasesnoves.swpro.movefast.vehicle.Vehicle;
+import java.time.LocalDate;
+import java.util.HashSet;
+
+/**
+ *
+ * @author Miquel
+ */
+public class Lloguer {
+
+    private LocalDate inici;
+    private LocalDate fi;
+    private HashSet<Vehicle> vehicles;
+    private HashSet<Client> clients;
+
+    public Lloguer(LocalDate inici, LocalDate fi) {
+        this.inici = inici;
+        this.fi = fi;
+        vehicles = new HashSet<>();
+        clients = new HashSet<>();
+    }
+
+    public HashSet<Vehicle> getVehicles() {
+        HashSet<Vehicle> copiaVehicle = new HashSet<>();
+
+        for (Vehicle copia : vehicles) {
+            copiaVehicle.add(copia);
+        }
+        return copiaVehicle;
+    }
+
+    public HashSet<Client> getClients() {
+
+        HashSet<Client> copiaClient = new HashSet<>();
+        for (Client copia : clients) {
+            copiaClient.add(copia);
+        }
+        return copiaClient;
+    }
+
+    public LocalDate getInici() {
+        return inici;
+    }
+
+    public LocalDate getFi() {
+        return fi;
+    }
+
+    public void setInici(LocalDate inici) {
+        this.inici = inici;
+    }
+
+    public void setFi(LocalDate fi) {
+        this.fi = fi;
+    }
+
+    public double calculaPreu() {
+        double preu = vehicle.getPreu();
+        if (client.getTipus() == TipusClient.LEASING) {
+            preu = preu * (10/100);
+        }
+        return preu;
+
+    }
+
+    public int compareTo(Lloguer lloguer) {
+        
+        return 0;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Lloguer{" + "inici=" + inici + ", fi=" + fi + ", vehicles=" + vehicles + ", clients=" + clients + '}';
+    }
+}
+
+//    public Vehicle vehiclesDisponibles(LocalDate dataConcreta) {
+//        
+//        for (Vehicle disponible : getVehicles()) {
+//            if (inici == dataConcreta) {
+//                if (Vehicle.disponible == true) {
+//                    return disponible;
+//                }                
+//            }
+//            
+//        }
+//        return null;
+//        
+//    }
+
