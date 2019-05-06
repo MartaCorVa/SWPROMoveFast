5704721e86d18208a0531738408d0c973a453f9c
maria <maria@gmail.com>
2019-05-06 18:57:46 +0200

    Miki

--- cat/iespaucasesnoves/swpro/movefast/vehicle/Marta.java
+++ cat/iespaucasesnoves/swpro/movefast/vehicle/Marta.java
@@ -12,6 +12,8 @@ public class Marta {
     public static void main(String[] args) {
         Cotxe c = new Cotxe(5);
         System.out.println(c);
+        
+        System.out.println("Miquel");
     }
     
 }
